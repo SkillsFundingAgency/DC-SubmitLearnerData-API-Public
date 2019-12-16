6800321e1b8850606a985556ee1f386b6925a124
Pete Chamley <peter@chamley.name>
2019-12-16 14:54:20 +0000

    Add Nuget Packages for Code Coverage

---
+++ src/ESFA.DC.SubmitLearnerData.API.Public.Tests/FakeUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ESFA.DC.SubmitLearnerData.API.Public.Tests
+{
+    class FakeUnitTest
+    {
+            [Fact]
+            public void Test()
+            {
+                Assert.True(true);
+            }
+    }
+}
